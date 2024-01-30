b2e0926ef9b4ba85445f25f8f2873f50058d3d17
burakgunce <brk.gnc9@hotmail.com>
2024-01-30 23:36:17 +0300

    ajax has been added for crud operations to use in different api

---
+++ Deneme_MVC/Controllers/AjaxController.cs
@@ -0,0 +1,38 @@
+﻿using ConsumeWebApiCors.Models.HospitalVMs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConsumeWebApiCors.Controllers
+{
+    public class AjaxController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GetAll()
+        {
+            return View();
+        }
+
+        public IActionResult GetAll2()
+        {
+            return View();
+        }
+
+
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(HospitalVM hospitalVM)
+        {
+            return View();
+        }
+
+        
+    }
+}
